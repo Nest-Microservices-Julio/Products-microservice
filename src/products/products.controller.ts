eb8931bacbb45e9b9fbe1765c72036f368e71d3c
Julio Donado <jdonado@unibol.com.co>
2024-04-25 16:12:26 -0500

    Se termino el REST

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, ParseIntPipe } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from '../common';
 
 @Controller('products')
 export class ProductsController {
@@ -9,21 +10,21 @@ export class ProductsController {
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
-    return createProductDto
+    return this.productsService.create(createProductDto);
   }
 
   @Get()
-  findAll() {
-    return this.productsService.findAll();
+  findAll( @Query() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
     return this.productsService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(@Param('id', ParseIntPipe) id: string, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.update(+id, updateProductDto);
   }
 
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from '../common';
 
 @Injectable()
-export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Connected to the database');
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async create(createProductDto: CreateProductDto) {
+    return this.product.create({
+      data: createProductDto,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findAll({ page, limit }: PaginationDto) {
+    const totalPages = await this.product.count({where: { available: true }});
+    const lastPage = Math.ceil(totalPages / limit);
+
+    return {
+      meta: {
+        page,
+        total: totalPages,
+        lastPage
+      },
+      data: await this.product.findMany({
+        take: limit,
+        skip: (page - 1) * limit,
+        where: { available: true },
+      }),
+    };
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findUnique({
+      where: { id, available: true },
+    })
+
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+
+    await this.findOne(id);
+
+    return this.product.update({
+      where: { id },
+      data: updateProductDto,
+    })
+  }
+
+  async remove(id: number) {
+
+    await this.findOne(id);
+
+    // return this.product.delete({
+    //   where: { id },
+    // });
+
+    const product = await this.product.update({
+      where: { id },
+      data: { available: false },
+    });
+
+    return product;
   }
 }
