f7798130cc51bf108c0b1415ec5af5188758ad16
Julio Donado <julio22567@hotmail.com>
2024-05-10 16:22:45 -0500

    feat: Add NATS messaging support for product validation

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import { envs } from './config';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const logger = new Logger('Main');
-  console.log(envs.port);
+  const logger = new Logger('Products-Microservice');
+  // console.log(envs.natsServers);
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port,
+        servers: envs.natsServers,
       },
     },
   );
